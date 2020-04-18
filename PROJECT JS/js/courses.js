29f67eaec4d1c9ca6cc53bdba8af38122b74663a
vl4d1s <vladismarkin@gmail.com>
2020-04-18 23:32:01 +0300

    new ver

--- PROJECT JS/js/courses.js
+++ PROJECT JS/js/courses.js
@@ -1,85 +1,81 @@
-
 class Courses {
-    constructor (page,data) {
-        this.page = page;
-        this.data = data;
-        this.show();
-        this.foo();
+  constructor(page, data) {
+    this.page = page;
+    this.data = data;
+    this.show();
+    this.canvasChart();
+  }
+
+  show() {
+    let corseData = JSON.parse(this.data);
+    let main = document.getElementById("main");
+    while (main.firstChild) {
+      main.removeChild(main.firstChild);
     }
-   
-    show () {
-        let corseData = JSON.parse(this.data);
-        let main = document.getElementById('main');
-        while (main.firstChild) {
-            main.removeChild(main.firstChild);
-        }
-        main.innerHTML = this.page;
-        let tbody = document.getElementById('tbody');
-        for (let i=0; i<corseData.length; i++) {
-            tbody.innerHTML += '<tr scope="row"> <td>' + 
-                corseData[i].code + '</td> <td>' +
-                corseData[i].student + '</td> <td>' +
-                corseData[i].examMark + '</td> <td>' +
-                corseData[i].projectMark + '</td> <td>' +
-                corseData[i].courseName + '</td> </tr>';
-        }
-        
+    main.innerHTML = this.page;
+    let tbody = document.getElementById("tbody");
+    for (let i = 0; i < corseData.length; i++) {
+      tbody.innerHTML +=
+        '<tr scope="row"> <td>' +
+        corseData[i].code +
+        "</td> <td>" +
+        corseData[i].student +
+        "</td> <td>" +
+        corseData[i].examMark +
+        "</td> <td>" +
+        corseData[i].projectMark +
+        "</td> <td>" +
+        corseData[i].courseName +
+        "</td> </tr>";
+    }
+  }
 
-        
+  getScoreColor = (score) => {
+    if (score >= 0 && score <= 50) {
+      return "red";
+    } else if (score > 50 && score <= 80) {
+      return "yellow";
+    } else if (score > 80 && score <= 99) {
+      return "green";
+    } else if (score === 100) {
+      return "blue";
+    }
+  };
+
+  canvasChart() {
+    let corseData = JSON.parse(this.data);
+    var dataPoints = [];
+    for (let i = 0; i < corseData.length; i++) {
+      dataPoints.push({
+        y: corseData[i].examMark,
+        label: corseData[i].courseName,
+        color: this.getScoreColor(parseInt(corseData[i].examMark)),
+      });
     }
 
-    getScoreColor = (score) =>{
-        if(score >= 0 && score <= 50){
-            return "red";
-        } else if( score > 50 && score <= 80){
-            return "yellow";
-        }else if(score > 80 && score <= 99){
-            return "green";
-        }else if (score ===100){
-            return "blue";
-        }
-    }
-
-    foo(){
-        let corseData = JSON.parse(this.data);
-        var dataPoints = [];
-        for(let i = 0; i < corseData.length; i++ ){
-            dataPoints.push({             
-                y: corseData[i].examMark,
-                label: corseData[i].courseName
-              });
-        }
-
-        console.log(dataPoints);
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-            animationEnabled: true,
-            
-            title:{
-                text:"My Courses"
-            },
-            axisX:{
-                interval: 1
-            },
-            axisY2:{
-                interlacedColor: "rgba(1,77,101,.2)",
-                gridColor: "rgba(1,77,101,.1)",
-                title: "My Grades"
-            },
-            data: [{
-                type: "bar",
-                name: "companies",
-                axisYType: "secondary",
-                color: [
-                    'rgb(0,255,0,1)',
-                    'rgb(0,0,255,1)',
-                    'rgb(0,255,0,1)'
-                ],
-                dataPoints: dataPoints
-            }]
-        });
-        chart.render();
-    }
-    
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      title: {
+        text: "My Courses",
+      },
+      axisX: {
+        interval: 1,
+      },
+      axisY2: {
+        interlacedColor: "rgba(1,77,101,.2)",
+        gridColor: "rgba(1,77,101,.1)",
+        title: "My Grades",
+      },
+      data: [
+        {
+          color: "LightSeaGreen",
+          type: "bar",
+          name: "companies",
+          axisYType: "secondary",
+          dataPoints: dataPoints,
+        },
+      ],
+    });
+    chart.render();
+  }
 }
-
